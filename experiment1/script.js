152fa4de710bed668e06d3d93405afd7debd0d54
armanares <zaddyarman@gmail.com>
2026-04-07 20:34:59 +0530

    commit changes

--- experiment1/script.js
+++ experiment1/script.js
@@ -1,28 +1,51 @@
-function CalculateResult() {
-    let n = document.getElementById("subjects").value;
-
+function calculateResult() {
+    let n=document.getElementById("subjects").value;
     let i;
     let total=0;
-    for(i=0;i<n;i++) {
-      let x=parseFloat(prompt("enter marks of subject"+(i+1)));
-      total+=x;
-    }
-    let average=total/n;
-    let grade;
-    if (average>90) {
-        grade="A+";
-    }
-    else if (average>80) {
-        grade="B+";
-    } 
-    else if (average>70) {
-        grade="C+";
-    }
-    else if (average>60) {
-        grade="D+";
-    }
-    else {
-        grade="F";
-    }
-    let result = document.getElementById("result").innerHTML="Total marks :" + total + "<br>Average: " + average + "<br>Grade: " + grade + "<br/>";
-}
+    for(i=0;i<n;i++)
+        {
+            let x=parseFloat(prompt("Enter the marks of subject "+(i+1)));
+            total+=x;
+        }
+        let avarage=total/n;
+        let grade;
+        if (avarage>90)
+        {
+            grade="A+";
+        }
+        else if (avarage>80)
+        {
+            grade="A";
+        }
+        else if (avarage>70)
+        {
+            grade="B";
+        }
+        else if (avarage>60)
+        {
+            grade="C";
+        }
+        else if (avarage>50)
+        {
+            grade="D";
+        }
+        else if (avarage>40)
+        {
+            grade="E";
+        }
+        else
+        {
+            grade="F";
+        }
+       
+       let result;
+         if (grade=="F")
+            {
+                result="Fail";
+            }   
+        else
+            {
+                result="Pass";
+            }
+        document.getElementById("result").innerHTML="Total Marks: "+total+"<br>Avarage Marks: "+avarage+"<br>Grade: "+grade+"<br>Result: "+result;
+}      
